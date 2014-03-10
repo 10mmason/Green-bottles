6637eba23ee2e2862626eccc7a18de7454ebacaf
10mmason <10mmason@ashmanorschool.com>
2014-03-10 11:50:20 +0000

    Update runtest.py

--- runtest.py
+++ runtest.py
@@ -1,1 +1,11 @@
-#nothing yet
+#in this file we shall tes bottle.py
+import unittest
+import bottle
+
+class tests (unittest.Case):
+    def test1(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
