9d82e17a9c6c36af896ceb99ed3c8bc351bf857a
10mmason <10mmason@ashmanorschool.com>
2014-03-11 14:16:51 +0000

    Update runtest.py

--- runtest.py
+++ runtest.py
@@ -13,3 +13,5 @@ if __name__ == '__main__':
 def test1(self):
     song_expected_last_word = 'floor'
     song_actual_last_word = bottles.song
+    self.assertEquals(song_expected_last_word, song_actual_last_word, "last word is wrong, Test 1 fails")
+
