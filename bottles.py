ba6d4d6f1dbfe525e572eae3a3aa51658b758b6a
10mmason <10mmason@ashmanorschool.com>
2014-03-11 14:36:05 +0000

    Update bottles.py

--- bottles.py
+++ bottles.py
@@ -2,8 +2,10 @@
 
 
 
-
 def test1(self):
     song_expected_last_word = 'floor'
     song_actual_last_word = bottles.song[-5:]
     self.assertEquals(son_expected_last_word, song_actaul_last_word, "last wor is wrong, Test 1 fails")
+
+print test1
+
