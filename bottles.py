3e5cc28fc6dc598e9d3845b00b63f31dfb231fe5
OpenShift guest <10mmason@ashmanorschool.com>
2014-03-12 05:59:17 -0400

    test update

--- bottles.py
+++ bottles.py
@@ -1,7 +1,5 @@
+song = 'floor'
 
 
 
-def test1(self):
-    song_expected_last_word = 'floor'
-    song_actual_last_word = bottles.song[-5:]
-    self.assertEquals(song_expected_last_word, song_actaul_last_word, "last word is wrong, Test 1 fails")
+
--- runtest.py
+++ runtest.py
@@ -1,10 +1,13 @@
-#in this file we shall test bottle.py
+#in this file we shall test bottles.py
 import unittest
 import bottles
 
 class tests (unittest.TestCase):
     def test1(self):
-        pass
+        song_expected_last_word = 'floor'
+        song_actual_last_word = bottles.song[-5:]
+        self.assertEquals(song_expected_last_word, song_actual_last_word, "last word is wrong, Test 1 fails")
+
 
 
 if __name__ == '__main__':
