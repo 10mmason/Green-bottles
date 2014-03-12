6983c365d3f98e14b10ef9416e693b3c10f136d0
OpenShift guest <10mmason@ashmanorschool.com>
2014-03-12 06:35:16 -0400

    next part

--- runtest.py
+++ runtest.py
@@ -17,5 +17,8 @@ def test2(self):
     assert index_of_5 > index_of_9 #"5 does not come after 9, Test 2 fails"
     assert index_of_5 < index_of_4 #"5 does not come before 5, Test 2 fails"
 
+def test3(self):
+    assert (' 0' in bottles.song) #0 is not in bottles.song, test 3 fails 
+    
 if __name__ == '__main__':
     unittest.main()
